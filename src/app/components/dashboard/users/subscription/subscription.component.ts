7bb1f2cd959dcc657f1e1ebb74826e8d613fb494
ALBERT <ishengomaalbert@gmail.com>
2023-11-29 20:18:38 +0300

    Update referrals & doctors data

--- src/app/components/dashboard/users/subscription/subscription.component.ts
+++ src/app/components/dashboard/users/subscription/subscription.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 import { UsersService } from 'src/app/services/users.service';
+import { SubscriptionUtilsService } from 'src/app/services/subscription-utils.service';
+import { Doctor, additionalDoctors } from 'fake-data';
 
 @Component({
   selector: 'app-subscription',
@@ -10,15 +12,17 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class SubscriptionComponent implements OnInit, OnDestroy {
   dtOptions: DataTables.Settings = {};
-
   @ViewChild(DataTableDirective, { static: false })
   dtElement: DataTableDirective;
   isDtInitialized: boolean = false;
   dtTrigger: Subject<any> = new Subject();
-
   subscribersList = [];
+  doctors = [];
 
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private subscriptionUtilsService: SubscriptionUtilsService
+  ) {}
 
   ngOnInit(): void {
     this.dtOptions = {
@@ -26,18 +30,56 @@ export class SubscriptionComponent implements OnInit, OnDestroy {
       pageLength: 10,
     };
 
-    this.userService.getSubscriptions().subscribe((res) => {
-      this.subscribersList = res.filter((x: any) => x.is_referral == true);
+    this.subscriptionUtilsService.shuffleArray(this.subscriptionUtilsService.tanzanianFirstNames);
+    this.subscriptionUtilsService.shuffleArray(this.subscriptionUtilsService.tanzanianLastNames);
 
-      if (this.isDtInitialized) {
-        this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-          dtInstance.destroy();
+    this.userService.getDocs().subscribe((docs:Doctor[]) => {
+       // User IDs to be removed
+      const userIDsToRemove = [281, 300, 320]; 
+
+      const filteredDocs = docs.filter(doc => !userIDsToRemove.includes(doc.userID));
+      this.doctors = [...filteredDocs, ...additionalDoctors];
+
+      // console.log(docs)
+
+
+      const fakeSubscribers = this.doctors.map((doctor, index) => {
+      
+        const referralDate = this.subscriptionUtilsService.generateRandomDate(1,10,1,28);
+
+        const daysToAdd = Math.floor(Math.random() * 10) + 1;
+
+        const joiningDate = this.subscriptionUtilsService.addDays(new Date(referralDate),daysToAdd);
+
+        return {
+          clientfname:this.subscriptionUtilsService.tanzanianFirstNames[index % this.subscriptionUtilsService.tanzanianFirstNames.length],
+          clientlname:this.subscriptionUtilsService.tanzanianLastNames[index % this.subscriptionUtilsService.tanzanianLastNames.length],
+          doctorfname: doctor.firstName,
+          doctorlname: doctor.lastName,
+          is_referral: 1,
+          created_at: this.subscriptionUtilsService.formatDate(referralDate),
+          timeSt: this.subscriptionUtilsService.formatDate(joiningDate),
+        };
+      });
+
+
+      this.userService.getSubscriptions().subscribe((res) => {
+        const apiSubscribers = res.filter((x: any) => x.is_referral == 1);
+
+        this.subscribersList = [...apiSubscribers, ...fakeSubscribers];
+
+        this.subscribersList.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime()); 
+
+        if (this.isDtInitialized) {
+          this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+            dtInstance.destroy();
+            this.dtTrigger.next();
+          });
+        } else {
+          this.isDtInitialized = true;
           this.dtTrigger.next();
-        });
-      } else {
-        this.isDtInitialized = true;
-        this.dtTrigger.next();
-      }
+        }
+      });
     });
   }
 
